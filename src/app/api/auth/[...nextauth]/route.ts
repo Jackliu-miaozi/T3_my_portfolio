49b8815b11f3c3973af28560efe5c696fbaa4362
jackliu <lzyujn@gmail.com>
2025-04-07 16:54:14 +0800

    refactor(auth): 注释掉未使用的edge运行时和authOptions配置
    为了简化代码并移除未使用的配置，注释掉了`runtime`和`authOptions`的相关代码。这些配置在当前项目中未被使用，移除它们可以提高代码的可读性和维护性。
    
    为什么同样是git提交的，却main和test分支有不同，使用git diff main test查看，把不同解决后可以编译成功了。

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,19 +2,19 @@ import { handlers } from "@/server/auth";
 
 export const { GET, POST } = handlers;
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 
-export const authOptions = {
-  useSecureCookies: true,
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: true
-      }
-    }
-  }
-}
+// export const authOptions = {
+//   useSecureCookies: true,
+//   cookies: {
+//     sessionToken: {
+//       name: `__Secure-next-auth.session-token`,
+//       options: {
+//         httpOnly: true,
+//         sameSite: 'lax',
+//         path: '/',
+//         secure: true
+//       }
+//     }
+//   }
+// }
