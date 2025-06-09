4b94ac8d1b8216e528b32408fa4d68174476de6d
jackliu <lzyujn@gmail.com>
2025-06-09 13:01:32 +0800

    feat: 更新页面元数据和缓存设置
    为关于我页面添加缓存设置，并更新布局文件的元数据配置，包括标题、描述、社交媒体分享信息等

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,51 @@ import { SessionProvider } from "next-auth/react";
 import { TRPCReactProvider } from "@/trpc/react";
 // import Script from 'next/script';
 
+
 export const metadata: Metadata = {
-  title: "页面丢失",
-  description: "页面丢失",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: "Jack's 主页 | 个人网站",
+  description: "欢迎来到刘正源的个人网站，这里有我的项目、文章和个人介绍。",
+  keywords: ["个人网站", "开发者", "技术博客", "项目展示"],
+  metadataBase: new URL(
+    "https://t3-my-portfolio-git-test-jackliumiaozis-projects.vercel.app/",
+  ),
+  openGraph: {
+    title: "Jack's 主页 | 个人网站",
+    description: "欢迎来到刘正源的个人网站，这里有我的项目、文章和个人介绍。",
+    url: "https://t3-my-portfolio.vercel.app/",
+    siteName: "Jack's 主页",
+    locale: "zh_CN",
+    type: "website",
+    images: [
+      {
+        url: "/avatar.png",
+      },
+    ],
+  },
+
+  // 这是Twitter卡片的元数据配置
+  // card: 定义Twitter卡片的显示样式，summary_large_image表示大图预览
+  // title: Twitter分享时显示的标题
+  // description: Twitter分享时显示的描述
+  // images: Twitter分享时显示的图片路径
+  twitter: {
+    card: "summary_large_image",
+    title: "Jack's 主页 | 个人网站",
+    description: "欢迎来到Jack的个人网站，这里有我的项目、文章和个人介绍。",
+    images: [
+      {
+        url: "/avatar.png",
+        width: 1200,
+        height: 630,
+        alt: "Jack's Website",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon-16x16.svg",
+    apple: "/apple-touch-icon.svg",
+  },
 };
 
 const geist = Geist({
